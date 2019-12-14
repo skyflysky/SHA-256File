755fe19a5bec281e6854d14d7aba88c3396817d3
skygd <skygd@compal>
2019-12-14 18:49:09 +0800

    自定义线程名

--- Ero SHA-256/src/sky/tool/sha256/Mainer.java
+++ Ero SHA-256/src/sky/tool/sha256/Mainer.java
@@ -1,15 +1,18 @@
+
 package sky.tool.sha256;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Mainer
 {
 	public static void main(String[] args)
 	{
-		ExecutorService threadPool = Executors.newCachedThreadPool();
-		Util util = new Util(args[0]);
-		for(int i = 0 ; i < Integer.valueOf(args[1]) ; i ++)
+		ExecutorService threadPool = Executors.newFixedThreadPool(Integer.valueOf(10),new SkyThreadFactory());
+		Util util = new Util("I:\\game");
+		for (int i = 0; i < Integer.valueOf(10); i++)
 		{
 			threadPool.execute(new Worker(util));
 		}
@@ -31,6 +34,30 @@ public class Mainer
 				e.printStackTrace();
 			}
 		}
-		
+	}
+
+	private static class SkyThreadFactory implements ThreadFactory
+	{
+		private static final AtomicInteger poolNumber = new AtomicInteger(1);
+		private final ThreadGroup group;
+		private final AtomicInteger threadNumber = new AtomicInteger(1);
+		private final String namePrefix;
+
+		SkyThreadFactory()
+		{
+			SecurityManager s = System.getSecurityManager();
+			group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
+			namePrefix = "sky-" + poolNumber.getAndIncrement();
+		}
+
+		public Thread newThread(Runnable r)
+		{
+			Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
+			if (t.isDaemon())
+				t.setDaemon(false);
+			if (t.getPriority() != Thread.NORM_PRIORITY)
+				t.setPriority(Thread.NORM_PRIORITY);
+			return t;
+		}
 	}
 }
